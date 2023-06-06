f175baf5020cb3c63305146a042573709f0511e1
Rich Green <rich@reneodlt.com>
2023-06-06 13:56:12 +0100

    first working version

---
+++ docker/truffle-config.js
@@ -0,0 +1,16 @@
+// truffle-config.js
+
+module.exports = {
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    },
+    compilers: {
+        solc: {
+            version: "0.8.3"
+        }
+    }
+};
