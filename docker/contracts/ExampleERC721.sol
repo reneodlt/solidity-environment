28260505c91d480f822845e2289c63c388047ed1
Rich Green <rich@reneodlt.com>
2023-06-07 09:54:00 +0100

    Now has linting

--- docker/contracts/ExampleERC721.sol
+++ docker/contracts/ExampleERC721.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ExampleERC721 is ERC721, Ownable {
     uint256 public tokenId;
--- docker/contracts/SimpleStorage.sol
+++ docker/contracts/SimpleStorage.sol
@@ -1,9 +1,9 @@
 // contracts/SimpleStorage.sol
 
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-    uint storedData;
+    uint private storedData;
 
     function set(uint x) public {
         storedData = x;
